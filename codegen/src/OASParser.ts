60652dbbe312f61c5896afd82d7a576c3361c861
Maximilian Taeschner <maximilian.taeschner@gmail.com>
2019-06-06 08:31:37 +0200

    chore: add todos

--- codegen/src/OASParser.ts
+++ codegen/src/OASParser.ts
@@ -8,7 +8,7 @@ export class OASParser {
     async parseOAS(sourceFile: string, destDir: string) {
         // TODO: Use generators to allow plug ins
         log('Validating OpenApi definition')
-        // TODO: Get rid of CodeGenContext and instead use typescript.module? 
+        // TODO: Get rid of CodeGenContext and instead use typescript.module?
         const context = new CodeGenContext(sourceFile, destDir, await SwaggerParser.bundle(sourceFile))
         await context.mkDir(destDir)
 
@@ -17,10 +17,12 @@ export class OASParser {
         await context.writeFile(swaggerOutPath, JSON.stringify(await SwaggerParser.validate(sourceFile), null, 2))
 
         log('Creating types')
+        // TODO: Use ComponentSchemaTypesGenerator
         const typeFactory = new TypeFactory()
         await typeFactory.createTypesFileFromComponentSchemas(context)
 
         log('Creating resources')
+        // TODO: Create SlushyResourcesGenerator
         const resourceFactory = new ResourceFactory()
         await resourceFactory.createResources(context)
     }
